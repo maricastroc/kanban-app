06217b0bc8970b3c5038e120d1a42be67867cdf3
mariana castro <marianacastrorc@gmail.com>
2025-11-16 17:42:11 -0300

    removing nextauth

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -6,55 +6,51 @@ import { GlobalStyle } from '@/styles/global'
 import { BoardsContextProvider } from '@/contexts/BoardsContext'
 import { Toaster } from 'react-hot-toast'
 import { AppProps } from 'next/app'
-import { SessionProvider } from 'next-auth/react'
-import 'react-datepicker/dist/react-datepicker.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <SessionProvider>
-      <ThemeProvider>
-        <ThemeContext.Consumer>
-          {(context) => {
-            if (!context) {
-              return null
-            }
+    <ThemeProvider>
+      <ThemeContext.Consumer>
+        {(context) => {
+          if (!context) {
+            return null
+          }
 
-            const { enableDarkMode } = context
+          const { enableDarkMode } = context
 
-            return (
-              <StyledThemeProvider
-                theme={enableDarkMode ? darkTheme : lightTheme}
-              >
-                <BoardsContextProvider>
-                  <Toaster
-                    toastOptions={{
+          return (
+            <StyledThemeProvider
+              theme={enableDarkMode ? darkTheme : lightTheme}
+            >
+              <BoardsContextProvider>
+                <Toaster
+                  toastOptions={{
+                    style: {
+                      backgroundColor: '#20212C',
+                      color: '#fff',
+                    },
+                    success: {
                       style: {
                         backgroundColor: '#20212C',
                         color: '#fff',
                       },
-                      success: {
-                        style: {
-                          backgroundColor: '#20212C',
-                          color: '#fff',
-                        },
+                    },
+                    error: {
+                      style: {
+                        backgroundColor: '#20212C',
+                        color: '#fff',
                       },
-                      error: {
-                        style: {
-                          backgroundColor: '#20212C',
-                          color: '#fff',
-                        },
-                      },
-                    }}
-                  />
-                  <Component {...pageProps} />
-                  <GlobalStyle />
-                </BoardsContextProvider>
-              </StyledThemeProvider>
-            )
-          }}
-        </ThemeContext.Consumer>
-      </ThemeProvider>
-    </SessionProvider>
+                    },
+                  }}
+                />
+                <Component {...pageProps} />
+                <GlobalStyle />
+              </BoardsContextProvider>
+            </StyledThemeProvider>
+          )
+        }}
+      </ThemeContext.Consumer>
+    </ThemeProvider>
   )
 }
 
