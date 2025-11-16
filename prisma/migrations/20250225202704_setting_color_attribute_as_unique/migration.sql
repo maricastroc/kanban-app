801c6b9bd54b86df583853012a717654e7f42812
mariana castro <marianacastrorc@gmail.com>
2025-11-16 18:11:22 -0300

    updating

--- prisma/migrations/20250225202704_setting_color_attribute_as_unique/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[color]` on the table `tags` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "tags_color_key" ON "tags"("color");
